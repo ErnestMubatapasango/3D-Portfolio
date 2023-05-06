b17ec839c9c0bcc28242c240e94ee489df23d613
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-05-06 09:52:08 +0200

    tailwindcss debug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {styles} from '../styles'
+import {navLinks}from '../constants'
+import { menu, close } from '../assets'
 
 const Navbar = () => {
+
+  const [active, setActive] = React.useState('')
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center fixed top-0 z-20 py-5`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link to='/' className='flex items-center gap-2' onClick={() => {setActive(""); window.scrollTo(0,0)}}>
+          <img src='ernest_dev.png' alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-2xl'>Enerst Mubz | <span>Dev</span></p>
+        </Link>
+      </div>
+    </nav>
   )
-}
+} 
 
 export default Navbar
