58d1d2afd34ccfb26569ce9f387540c0debd4744
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-05-08 12:23:45 +0200

    contact form done

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-
+import {Suspense} from 'react'
+import {Canvas, useFrame} from '@react-three/fiber'
+import { Points, PointMaterial, Preload } from '@react-three/drei'
+import * as random from 'maath/random/dist/'
 const Stars = () => {
   return (
     <div>Stars</div>
